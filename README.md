9ac1c49f1a114b3e8a99b799aa0a6252d750bd75
Amelia <114128556+Ames-hub@users.noreply.github.com>
2023-10-02 23:07:56 +0800

    alpha

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # pyhoster
- An nginx replacement built with python to manage multiple website instances.
+An nginx replacement built with python to manage multiple website instances.
+
+## Installation
+Install python3.11 from the website or your package manager. (Such as MS store)
+then run these commands in the directory you want to install pyhoster in.
+```
+git clone https://github.com/Ames-hub/pyhoster .
+python3.11 -m venv venv
+```
+on windows, use `venv\Scripts\activate.bat` to activate the virtual environment.
+on linux, use `source venv/bin/activate` to activate the virtual environment.
+
+then run `pip install -r requirements.txt` to install what is needed.
+
+Then run `python3.11 pyhost.py -O` to start the program.
+
+# Environment variables
+PYHOST_KEYBIND_LISTEN: Boolean, if true, pyhoster will listen for keybinds. (Default: True)
+made toggleable for 'edge' cases where you want to use the keybinds for something else.
+
+# Keybinds/Keywords
+if a key is in brackets, I am indicating its on the keyboard and not to be typed out
+`create + [enter]` - Create a new website instance.
+`delete + [enter]` - Delete a website instance.
--- library/instance.py
+++ library/instance.py
@@ -16,17 +16,36 @@ class instance: # Do not use apptype in calls until other apptypes are made
                     tried = True
                 continue
             
-            port: int = int(input("What port should the app run on? NUMBER : "))
-            
+            try:
+                port: int = input("What port should the app run on? NUMBER (Default: 80) : ")
+                if str(port) == "":
+                    port = 80
+                assert type(port) is int, "The port must be an integer!"
+                port = int(port)
+                assert port > 0 and port < 65535, "The port must be between 0 and 65535!"
+            except AssertionError as err: # Forces the port to be valid
+                print(str(err))
+                continue
+
             try:
                 boundpath: str = str(input("What is the full path to the app's content? TEXT : "))
-                assert os.path.exists(boundpath) and os.path.isabs(boundpath), "The path must exist and be absolute!"
+                assert os.path.exists(boundpath) and os.path.isabs(boundpath), "The path must exist and be absolute! (absolute: starting from root directory such as C:/)"
             except AssertionError as err: # Forces the path to be valid and absolute
                 print(str(err))
                 continue
-            assert boundpath is os.path.abspath(boundpath), "The path must be absolute!"
 
-            do_autostart: bool = bool(input("Should the app autostart? Y/N : "))
+            try:
+                do_autostart: str = input("Should the app autostart? Y/N : ").lower()
+                if "y" in do_autostart:
+                    do_autostart = True
+                elif "n" in do_autostart:
+                    do_autostart = False
+                else:
+                    raise AssertionError("The autostart must be either 'Y' or 'N'!")
+                assert type(do_autostart) is bool, "The autostart must be a boolean!"
+            except AssertionError as err: # Forces the autostart to be valid
+                print(str(err))
+                continue
             break
 
         # Makes the appropriate directories
