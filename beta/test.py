a51917cd82d48fa6454745eddae771175d979477
Amelia <114128556+Ames-hub@users.noreply.github.com>
2023-10-04 18:01:35 +0800

    alpha

--- beta/test.py
+++ beta/test.py
@@ -1,6 +1,18 @@
-import http.server, socketserver, os, datetime, sys
+import http.server
+import socketserver
+import os
+import datetime
+import sys
+import threading
 
 def start_custom_web_server(port, directory, app_name, silent=True):
+    '''
+    A Function which can initialize a already created webserver folder and serve the files.
+    port: The port to serve the files on
+    directory: The directory to serve the files from. (Auto normally, but can be overriden here.)
+    app_name: The name of the app to be used in the logs
+    '''
+    os.chdir(directory)
     class CustomHandler(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
             # Log the request action to a log file
@@ -22,8 +34,10 @@ def start_custom_web_server(port, directory, app_name, silent=True):
                 else:
                     log_file.write(f"{datetime.datetime.now()} - {app_name} - IP {client_address} requested file {requested_file}\n")
 
-    # Change to the specified directory and serve from there
-    os.chdir(directory)
+    def log_message(message):
+        current_date = datetime.date.today().strftime("%Y-%m-%d")
+        with open(f"logs/{current_date}.log", "a") as log_file:
+            log_file.write(f"{datetime.datetime.now()} - {app_name} - {message}\n")
 
     # Create the log directory if it doesn't exist
     log_directory = f"logs/"
@@ -38,13 +52,28 @@ def start_custom_web_server(port, directory, app_name, silent=True):
     with socketserver.TCPServer(("", port), CustomHandler) as httpd:
         # Print a message to indicate the server has started unless silent is True
         if not silent:
-            print(f"Server is running. Check the logs for actions.")
+            print(f"Server \"{app_name}\" is running. Check the logs for actions.")
+            print(f"You can visit it on http://localhost:{port}")
+        log_message(f"Server \"{app_name}\" is running.")
 
         # Start the server and keep it running until interrupted
         try:
             httpd.serve_forever()
         except KeyboardInterrupt:
-            print("\nServer stopped.")
+            if not silent: print(f"\nServer \"{app_name}\" stopped.")
+            log_message(f"Server \"{app_name}\" stopped.")
 
-start_custom_web_server(8080, "./beta/testweb", "mytestapp", silent=True)
+# Use absolute paths for both directories
+directory1 = os.path.abspath("./beta/testweb")
+directory2 = os.path.abspath("./beta/testweb2")
+
+# Run two instances of the web server concurrently
+threading.Thread(
+    target=start_custom_web_server,
+    args=(8080, directory1, "mytestapp", False)).start()
+
+threading.Thread(
+    target=start_custom_web_server,
+    args=(8081, directory2, "mytestapp2", False)).start()
+
 # A Function I am developing to replace nginx. Not working on main app for now
