ce437e9393fe2de2e04ae6475b14a72a6523db21
Amelia <114128556+Ames-hub@users.noreply.github.com>
2023-12-15 19:21:51 +0800

    Update title in index.html and add status_detector.js script

---
+++ library/WebGUI/content/javascript/status_detector.js
@@ -0,0 +1,43 @@
+let status_bar = document.getElementById('status_detector');
+let status_text = document.getElementById('status_text');
+
+function updateStatus(status_bar, status_text) {
+    fetch("http://localhost:987", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            "username": "Ame",
+            "password": "HellWillFearMyWrath"  
+        })
+    })
+    .then(response => response.text()) // convert response to text
+    .then(data => {
+        console.log(data)
+        console.log(data == "Pong!")
+        if (data == "Pong!") {
+            status_bar.style.backgroundColor = "green";
+            status_text.innerHTML = "Online";
+            return true;
+        }
+        else {
+            status_bar.style.backgroundColor = "red";
+            status_text.innerHTML = "Offline";
+            return false;
+        }
+    })
+    .catch(() => {
+        status_bar.style.backgroundColor = "red";
+        status_text.innerHTML = "Offline";
+        return false;
+    });
+}
+
+let delay = 3000;
+updateStatus(status_bar, status_text);
+setInterval(function() {
+    if (!updateStatus(status_bar, status_text)) {
+        delay = 30000; // Backoff if server is offline
+    }
+}, delay);
